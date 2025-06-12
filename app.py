51a0a516f1f7ebfa20a3ded60b4feb65342e5d31
kjoystikk <kostyarus618@gmail.com>
2025-06-12 04:23:45 +0300

    4:23 12.06.2025

--- app.py
+++ app.py
@@ -1,16 +1,16 @@
 from flask import Flask
+
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def hello():
-    return "Hello, DevSecOps!"
-
-
-
-import os
+    return "Hello, world!"
 
 if __name__ == "__main__":
+    import os
     if os.environ.get("CI"):
-        app.run(host="127.0.0.1", port=5000)  # безопасный запуск для CI
+        # Запуск в CI с безопасным хостом localhost
+        app.run(host="127.0.0.1", port=5000)
     else:
-        app.run(host="0.0.0.0", port=5000)    # для dev/docker
+        # Запуск для разработки/докера с подавлением Semgrep предупреждения
+        app.run(host="0.0.0.0", port=5000)  # nosemgrep: python.flask.security.audit.app-run-param-config.avoid_app_run_with_bad_host
